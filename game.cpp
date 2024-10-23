29121cafb2ece3df4a7c553ec606986759b03eb6
faizr93 <faizr9315@gmail.com>
2024-10-23 19:20:37 +0500

    Added Food Generation Added Collision Detection Minor Improvements Major Bugs Will be Fixed in Next Version

--- game.cpp
+++ game.cpp
@@ -1,49 +1,92 @@
 #include "game.h"
-#include <iostream>
-#include <vector>
-// Constructor & Destructor
+#include <unistd.h>
+
+// Constructor
 Game::Game()
 {
     this->initVariables();
     this->initWindow();
     this->initSnakeSegments();
+    this->initFood();
 }
-
+// Destructor
 Game::~Game()
 {
     delete this->window;
-    delete this->snake_head;
+    delete this->snake_segment;
     delete this->video_mode;
     delete this->clock;
 }
 
 // Private Functions
+
+// Initializes Variables of Class Game
 void Game::initVariables()
 {
     this->window = nullptr;
-    this->snake_head = nullptr;
+    this->snake_segment = nullptr;
     this->dir = UP;
     this->clock = new sf::Clock;
 }
 
+// Initializes Window with Const Width and Height, Sets FrameRate Limit.
 void Game::initWindow()
 {
     this->video_mode = new sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT);
     this->window = new sf::RenderWindow(*this->video_mode, "MyGame");
     this->window->setFramerateLimit(75);
 }
+
+// Initializes All Parts of the Snake such as Head, Middle, Tail
+// Initializes Their Color, Size and Position
 void Game::initSnakeSegments()
 {
     //     initSnakeHead();
     //     initSnakeMidBody();
     //     initSnakeTail();
-    this->snake_head = new sf::RectangleShape;
+    this->snake_segment = new sf::RectangleShape;
+    snake_segment->setOutlineThickness(3.f);
+    snake_segment->setSize(sf::Vector2f(GRID_CELL_SIZE - snake_segment->getOutlineThickness(), GRID_CELL_SIZE - snake_segment->getOutlineThickness()));
+    snake_segment->setFillColor(sf::Color::Green);
+    snake_segment->setOutlineColor(sf::Color::Transparent);
+    snake_segment->setPosition(sf::Vector2f(150.f, 150.f));
+}
+void Game::initFood()
+{
+    this->food = new sf::RectangleShape;
+    food->setOutlineThickness(3.f);
+    food->setSize(sf::Vector2f(GRID_CELL_SIZE - food->getOutlineThickness(), GRID_CELL_SIZE - food->getOutlineThickness()));
+    food->setFillColor(sf::Color::Red);
+    food->setOutlineColor(sf::Color::Black);
+    food->setPosition(sf::Vector2f(150.f, 150.f));
+}
 
-    snake_head->setSize(sf::Vector2f(GRID_CELL_SIZE, GRID_CELL_SIZE));
-    snake_head->setFillColor(sf::Color::Green);
-    snake_head->setOutlineThickness(3.f);
-    snake_head->setOutlineColor(sf::Color::Black);
-    snake_head->setPosition(sf::Vector2f(150.f, 150.f));
+void Game::generateFood()
+{
+    int grid_width = static_cast<int>(WINDOW_WIDTH / GRID_CELL_SIZE);
+    int grid_height = static_cast<int>(WINDOW_HEIGHT / GRID_CELL_SIZE);
+
+    // Ensure food doesn't spawn on snake position
+    sf::Vector2f food_position;
+    bool valid_food_position = false;
+
+    while (!valid_food_position)
+    {
+        int x = (rand() % grid_width) * GRID_CELL_SIZE;
+        int y = (rand() % grid_height) * GRID_CELL_SIZE;
+        food_position = sf::Vector2f(static_cast<float>(x), static_cast<float>(y));
+
+        valid_food_position = true; // Assume valid until we check against all snake positions
+        for (const auto &position : vec_snake_positions)
+        {
+            if (position == food_position)
+            {
+                valid_food_position = false; // Found a collision with the snake
+                break;
+            }
+        }
+    }
+    food->setPosition(food_position);
 }
 
 // Accessors
@@ -52,98 +95,121 @@ const bool Game::isRunning() const
     return this->window->isOpen();
 }
 
-// Functions
+// Public Functions
 
+// Tracks All Keypresses (events) and Stores them in Event Variable (ev)
+// Takes Appropriate Action based on each Event
 void Game::pollEvents()
 {
     while (this->window->pollEvent(ev))
     {
         switch (ev.type)
         {
-            // Making the Window Closeable
+        // Making the Window Closeable
         case sf::Event::Closed:
             this->window->close();
             break;
+
         case sf::Event::KeyPressed:
-            if (ev.key.code == sf::Keyboard::Escape)
-            {
-                this->window->close();
-            }
-
-            // Direction Changing Logic
-            if (ev.key.code == sf::Keyboard::Up)
-            {
-                dir = UP;
-            }
-
-            if (ev.key.code == sf::Keyboard::Down)
-            {
-                dir = DOWN;
-            }
-
-            if (ev.key.code == sf::Keyboard::Right)
-            {
-                dir = RIGHT;
-            }
-
-            if (ev.key.code == sf::Keyboard::Left)
-            {
-                dir = LEFT;
-            }
+            setDirection();
             break;
         }
     }
 }
 
-void Game::moveSnakeHead(float x, float y)
-{
-    this->snake_head->move(sf::Vector2f(x, y));
-}
+// Sets Direction(dir) to KeyPressed
+void Game::setDirection() // clang-format off
+{  
+    switch (ev.key.code) {
+        case sf::Keyboard::Up:    dir = UP;    break;
+        case sf::Keyboard::Down:  dir = DOWN;  break;
+        case sf::Keyboard::Right: dir = RIGHT; break;
+        case sf::Keyboard::Left:  dir = LEFT;  break; } 
+} 
+
+// Moves Snake Head Based On Dir (Direction Variable)
 void Game::moveSnake()
 {
-    if (dir == UP)
-    {
-        moveSnakeHead(0, -GRID_CELL_SIZE);
-    }
-    if (dir == DOWN)
-    {
-        moveSnakeHead(0, GRID_CELL_SIZE);
-    }
-    if (dir == RIGHT)
-    {
-        moveSnakeHead(GRID_CELL_SIZE, 0);
-    }
-    if (dir == LEFT)
-    {
-        moveSnakeHead(-GRID_CELL_SIZE, 0);
-    }
+    switch(dir) {
+        case UP:    moveSnakeHead(0, -GRID_CELL_SIZE); break; // Negative Y value = up in SFML
+        case DOWN:  moveSnakeHead(0,  GRID_CELL_SIZE); break;
+        case RIGHT: moveSnakeHead( GRID_CELL_SIZE, 0); break;
+        case LEFT:  moveSnakeHead(-GRID_CELL_SIZE, 0); break; }
+    // clang-format on
 }
+
+// Encapsulation of the Move Function
+// Moves Snake Head by offset of given Float Parameters
+void Game::moveSnakeHead(float x, float y)
+{
+    this->snake_segment->move(sf::Vector2f(x, y));
+}
+
+/* 
+Checks Each Position in Storing Vector against Position to be moved to
+Returns True if Equal
+Returns False if To Be moved Position is a new one.
+*/
+bool Game::isSelfCollided()
+{
+    for (const auto &position : vec_snake_positions)
+    {
+        if (position == snake_pos)
+        {
+            return true;
+        }
+        break;
+    }
+    return false;
+}
+
+bool Game::isFoodCollided()
+{
+    if (snake_pos == food->getPosition()){
+        return true;
+    }
+    return false;
+}
+
+
+// All Game Logic, Runs in Intervals of Given Amount
 void Game::update()
 {
-    if (clock->getElapsedTime().asMilliseconds() >= 500.0f)
+    if (clock->getElapsedTime().asMilliseconds() >= 400.0f)
     {
-
-        this->moveSnake();
-        sf::Vector2f snake_pos = this->snake_head->getPosition();
-        if (snake_pos.x > (500.f-GRID_CELL_SIZE))
+        moveSnake();
+        wrapSnakePos();
+        snake_pos = this->snake_segment->getPosition(); // Get Snake's Position
+        if (isSelfCollided())
         {
-            this->snake_head->setPosition(0, snake_pos.y);
+            vec_snake_positions.clear();
+            snake_length = 3;
         }
-        if (snake_pos.y > (500.f-GRID_CELL_SIZE)){
-            this->snake_head->setPosition(snake_pos.x,0);
-        };
-        if (snake_pos.x < 0){
-            this->snake_head->setPosition((500.f-GRID_CELL_SIZE), snake_pos.y);
+        if (isFoodCollided()) {
+            generateFood();
+            snake_length++;
         }
-        if (snake_pos.y < 0){
-            this->snake_head->setPosition(snake_pos.x,(500.f-GRID_CELL_SIZE));
-        };
-        std::cout << "Snake Position: (" << snake_pos.x << ", " << snake_pos.y << ")" << std::endl;
+        vec_snake_positions.push_back(snake_pos); // Store Snake's Position
+
+        if (vec_snake_positions.size() > snake_length)
+        {
+            vec_snake_positions.erase(vec_snake_positions.begin()); // Remove the oldest position
+        }
+        
         clock->restart(); // Reset the clock
     }
     this->pollEvents();
 }
 
+void Game::wrapSnakePos()
+{
+    sf::Vector2f snake_pos = this->snake_segment->getPosition();  // Get Pos
+    snake_pos.x = fmod(snake_pos.x + WINDOW_WIDTH, WINDOW_WIDTH); // Wrap Pos
+    snake_pos.y = fmod(snake_pos.y + WINDOW_HEIGHT, WINDOW_HEIGHT);
+
+    this->snake_segment->setPosition(snake_pos);                                                // Set New Pos
+    std::cout << "Snake Position: (" << snake_pos.x << ", " << snake_pos.y << ")" << std::endl; // TEMP DEBUG LINE
+}
 /*
     @return void
 
@@ -157,15 +223,19 @@ void Game::update()
 */
 void Game::render()
 {
-    this->window->clear();
-
-    // Draw game objects
-    this->window->draw(*this->snake_head);
-
-    this->window->display();
+    this->window->clear(); // Clear Old Frame
+    // Draw game objects to / Create New Frame
+    for (auto &position : vec_snake_positions)
+    {
+        this->snake_segment->setPosition(position);
+        this->window->draw(*this->snake_segment);
+        this->window->draw(*this->food);
+    }
+    this->window->display(); // Display the New Frame
 }
 
 /*
-Future Reference ( I used pointers, The reasons are explained below, Use this video If i forget how SFML or C++ functions.
+Future Reference ( I used pointers, The reasons are explained below, Use this video If i forget how SFML or C++ work.
 https://www.youtube.com/watch?v=kxb0GvBNOGU&list=PL6xSOsbVA1eb_QqMTTcql_3PdOiE928up&index=6
 */
+
--- game.h
+++ game.h
@@ -2,7 +2,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <conio.h>
 
 /*
     Class that acts as Game Engine
@@ -14,26 +17,32 @@ private:
     // Window and Other Class Objects
     sf::RenderWindow *window; // without pointers, the object gets destroyed after functions completion.
     sf::Event ev;
-    sf::RectangleShape *snake_head;
+    sf::RectangleShape *snake_segment; //A Single Snake Segment, with color, position
+    sf::RectangleShape *food;
     sf::VideoMode *video_mode;
     sf::Clock *clock;
-    // Variables
-    const char UP = 'w';
-    const char DOWN = 's';
-    const char RIGHT = 'd';
-    const char LEFT = 'a';
-    const float GRID_CELL_SIZE = 25;
-    const uint32_t WINDOW_HEIGHT = 500;
-    const uint32_t WINDOW_WIDTH = 500;
     
+    sf::Vector2f snake_pos; // The Snake Segments Current Position Stored in Vector2f
+    std::vector<sf::Vector2f> vec_snake_positions; //A Vector(dynamic array) Storing Snake's Previous Positions as vector2f
+    // Variables
+    static const char UP = 'w';
+    static const char DOWN = 's';
+    static const char RIGHT = 'd';
+    static const char LEFT = 'a';
+    static const uint32_t WINDOW_HEIGHT = 500;
+    static const uint32_t WINDOW_WIDTH = 500;
+    const float GRID_CELL_SIZE = 25;
     char dir;
+    int snake_length = 1550;
 
     // Private Functions.
     void initVariables();
     void initWindow();
     void initSnakeSegments();
-    void moveSnakeHead(float x, float y);
-    void moveSnake();
+    void initFood();
+    bool isSelfCollided();
+    bool isFoodCollided();
+    
 
 public:
     // Constructors & Destructors
@@ -44,6 +53,11 @@ public:
     const bool isRunning() const;
 
     // Functions
+    void generateFood();
+    void moveSnakeHead(float x, float y);
+    void moveSnake();
+    void setDirection();
+    void wrapSnakePos();
     void pollEvents();
     void update();
     void render();
